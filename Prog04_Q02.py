f7abca78d65dce4f07afbc43bd1248cf1602c8fd
PhotcharaPhoempool <6610301008@cdti.ac.th>
2025-12-07 14:26:05 +0700

    Prog04

---
+++ Prog04_Q02.py
@@ -0,0 +1,50 @@
+def myrun(N):
+    '''Student code begins here'''
+    f = lambda x: x**2 -4
+
+    # แถวทั่วไป
+    p = lambda i, L, U, M, Ea, m: print(
+        f"{i:5d}     {L:.5f} {U:.5f} {M:.5f} {Ea:7.4f} {f(M):10.3e} {m}"
+    )
+
+    U, L = 2.4, 1.7
+    oldM = (U + L) / 2
+
+    print("Iteration    Xl      Xu      Xm     |Ea|     f(Xm)   m")
+    print("======================================================")
+
+    def calM(E):
+        m, T = 0, 5
+        while T > E:
+            m += 1
+            T /= 10
+        return m
+    
+    for i in range(N):
+        if i == 0:
+            # แก้ตรงนี้: ลดช่องว่างหลัง "--" จาก 5 เป็น 4 ช่อง
+            print(f"{i+1:5d}     {L:.5f} {U:.5f} {oldM:.5f}   --    {f(oldM):10.3e} -")
+            #                                      ^^^^
+            continue
+
+        if f(L)*f(oldM) < 0:
+            U = oldM
+        else:
+            L = oldM
+
+        M = (U + L) / 2
+        Ea = abs((M - oldM)/M) * 100
+        m = calM(Ea)
+        p(i+1, L, U, M, Ea, m)
+        oldM = M
+
+        if m >= 4:
+            break
+
+    '''Student code ends here'''
+
+
+if __name__=='__main__':
+    N = int(input("Enter a positive integer: "))
+    print("======================================================")
+    myrun(N)
